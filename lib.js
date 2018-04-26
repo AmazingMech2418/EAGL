a792c3e3dac22bc36161106e2acca1150b626271
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-26 19:52:51 -0400

    Update lib.js

--- lib.js
+++ lib.js
@@ -9,3 +9,21 @@ process.chdir(dir);
 function getDir() {
 return process.cwd;
 }
+
+let fs = require('fs');
+function read(file) {
+fs.readFileSync(file,{encoding:"utf-8"});
+}
+function write(file,content) {
+fs.writeFileSync(file,content);
+}
+
+var http = require('http');
+var url = require('url');
+function server(content,port) {
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  res.end(content);
+}).listen(port);
+}
