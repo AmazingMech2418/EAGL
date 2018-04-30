c14baabd8d3af906365a8d269844fafb0b030f3a
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-30 18:14:39 -0400

    Create canvas.js

---
+++ src/modules/canvas.js
@@ -0,0 +1,4 @@
+module("add html",function(args){addTag(args[0]);});
+module("add javascript",function(args){addjavascript(args[0]);});
+module("run javascript",function(args){return runjavascript(args[0]);});
+module("compile canvas for graphics interface",function(args){compileCanvas();});
