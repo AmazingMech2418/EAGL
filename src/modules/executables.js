4fc2dc912fd30483c7dd580e75b21eb595b64aa9
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-30 18:12:20 -0400

    Update executables.js

--- src/modules/executables.js
+++ src/modules/executables.js
@@ -1,1 +1,8 @@
-
+module("run batch or bash",function(args){exec(args[0]);});
+module("open a file",function(args){open(args[0]);});
+module("change directory",function(args){cd(args[0]);});
+module("get directory",function(args){return getDir()();});
+module("write to a file",function(args){write(args[0],args[1]);});
+module("read a file",function(args){return read(args[0]);});
+module("start a server",function(args){server(args[0],args[1]);});
+module("start a server and open it",function(args){server(args[0],args[1]);open("http://localhost:"+args[1]);});
